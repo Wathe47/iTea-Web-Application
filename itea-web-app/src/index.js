7e6a88e011498d8f638b8184e14c40621f46e55e
Wathe47 <wathsalyagamage23@gmail.com>
2024-02-01 15:46:10 +0530

    Change the Signing URL to Choreo's

--- itea-web-app/src/index.js
+++ itea-web-app/src/index.js
@@ -5,13 +5,27 @@ import App from "./App";
 
 const Index = () => (
     <AuthProvider
+
+    // config={ {
+    //   signInRedirectURL: "http://localhost:3000",
+    //   signOutRedirectURL: "http://localhost:3000",
+    //   clientID: "jweDabmkfrTPx2oQaSgEBijWQXUa",
+    //   baseUrl: "https://api.asgardeo.io/t/wathsalyagamage",
+    //   scope: [ "openid","profile" ]
+    // } }
+
     config={ {
-      signInRedirectURL: "http://localhost:3000",
-      signOutRedirectURL: "http://localhost:3000",
-      clientID: "jweDabmkfrTPx2oQaSgEBijWQXUa",
-      baseUrl: "https://api.asgardeo.io/t/wathsalyagamage",
-      scope: [ "openid","profile" ]
-  } }
+        signInRedirectURL: "https://420d584e-dd02-49b1-879e-78821d099417.e1-us-east-azure.choreoapps.dev/",
+        signOutRedirectURL: "https://420d584e-dd02-49b1-879e-78821d099417.e1-us-east-azure.choreoapps.dev/",
+        clientID: "jweDabmkfrTPx2oQaSgEBijWQXUa",
+        baseUrl: "https://api.asgardeo.io/t/wathsalyagamage",
+        scope: [ "openid","profile" ]
+    } }
+
+
+
+
+
     >
         <App/>
     </AuthProvider>
